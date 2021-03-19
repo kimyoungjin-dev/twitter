c37fbaebad32f3d0c4f4b45bf7bc389bdc529e81
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-19 23:32:34 +0900

    After pressing the logout button, use Redirect in react-router-dom to move to the home.

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "Routes/Home";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Login from "Routes/Login";
+import Home from "Routes/Home";
+import Profile from "Routes/Profile";
+import Navigation from "./Navigation";
 
 export default ({ isLogin }) => {
   return (
     <Router>
+      {isLogin && <Navigation />}
       <Switch>
         {isLogin ? (
-          <Route exact component={Home} path="/" />
+          <>
+            <Route exact component={Home} path="/" />
+            <Route exact component={Profile} path="/profile" />
+            <Redirect from="*" to="/" />
+          </>
         ) : (
-          <Route component={Login} path="/" />
+          <>
+            <Route component={Login} path="/" />
+            <Redirect from="*" to="/" />
+          </>
         )}
       </Switch>
     </Router>
