fc37e5c67c52ca8f5b104819550c15a95d97aa9a
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-20 01:23:25 +0900

    Post a comment comment, and send the tweet information to tweet .js.

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
+import Tweet from "Components/Tweet";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
-  const [tweets, setTweets] = useState([]);
-  console.log(tweets);
+  const [tweets, setTweets] = useState([]); //tweets는 문서의 아이디를 가지고있고 세부정보 값들을 가지고있음
   const getData = async () => {
-    const dbTweet = await dbService.collection("tweet").get();
-    dbTweet.forEach((document) => {
-      const tweetObj = {
-        ...document.data(),
-        id: document.id,
-      };
-      setTweets((prev) => [tweetObj, ...prev]);
+    dbService.collection("tweet").onSnapshot((snapshot) => {
+      const tweetArray = snapshot.docs.map((e) => ({
+        id: e.id, //id 는 creatorId 가아닌 tweet의 document 아이디를 의미
+        ...e.data(), //나머지 데이터는 collection 안의 정보를 의미
+      }));
+      console.log(tweetArray);
+      setTweets(tweetArray);
     });
   };
 
@@ -30,8 +30,9 @@ const Home = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("tweet").add({
-      tweet,
-      createBy: Date.now(),
+      text: tweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
     });
     setTweet("");
   };
@@ -44,12 +45,17 @@ const Home = () => {
           placeholder="당신의 트윗을 적어보세요"
           maxLength={120}
           onChange={onChange}
+          value={tweet}
         />
-        <input type="submit" value="트윗보내기" />
+        <input type="submit" value="트윗보내기" style={{ marginBottom: 20 }} />
       </form>
       <div>
-        {tweets.map((e) => (
-          <div key={e.id}>{e.tweet}</div>
+        {tweets.map((tweet) => (
+          <Tweet
+            key={tweet.id}
+            tweetObj={tweet}
+            isOwer={tweet.creatorId === userObj.uid}
+          />
         ))}
       </div>
     </div>
