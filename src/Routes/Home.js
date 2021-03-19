8ee1d0fae666a8b02487bbba66fc09e7dfa10d48
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-19 23:33:56 +0900

    Work on the home screen. Initial state work with onchange, onsubmit and usestate

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  return <>Home</>;
+  const [tweet, setTweet] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="당신의 트윗을 적어보세요"
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type="submit" value="트윗보내기" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
