b7d4ad0d58c9c0dbe7a2e9f52797445de52c1e5e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-20 01:27:26 +0900

    Clear isLogin, if userobj is present, login will be done, otherwise, login will not be done.

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,23 +5,25 @@ import Loading from "Components/Loading";
 
 const App = () => {
   const [init, setInit] = useState(false);
-  const [isLogin, setIsLogin] = useState(false);
   const [userObj, setUserObj] = useState(null);
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLogin(true);
         setUserObj(user);
-      } else {
-        setIsLogin(false);
       }
       setInit(true);
     });
   }, []);
 
   return (
-    <>{init ? <Router isLogin={isLogin} userObj={userObj} /> : <Loading />}</>
+    <>
+      {init ? (
+        <Router isLogin={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
 
