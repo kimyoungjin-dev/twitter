85163bd901ffa776ebdb6e341d4fb4a228d71c8f
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-20 02:23:13 +0900

    Create Files
    Create a usestate containing the attachment url, and convert the image selected by me to "data_url" through new FileReader () with a function called onFileChage. A function called onClearPhotoClick is an onclick attribute for a button that deletes a photo.

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
+import { storageService } from "fbase";
 import { dbService } from "fbase";
 import Tweet from "Components/Tweet";
+import { v4 as uuid } from "uuid";
 
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]); //tweets는 문서의 아이디를 가지고있고 세부정보 값들을 가지고있음
+  const [attachment, setAttachment] = useState("");
+
   const getData = async () => {
     dbService.collection("tweet").onSnapshot((snapshot) => {
       const tweetArray = snapshot.docs.map((e) => ({
         id: e.id, //id 는 creatorId 가아닌 tweet의 document 아이디를 의미
         ...e.data(), //나머지 데이터는 collection 안의 정보를 의미
       }));
-      console.log(tweetArray);
       setTweets(tweetArray);
     });
   };
@@ -29,13 +32,38 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection("tweet").add({
-      text: tweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
-    setTweet("");
+    const reference = storageService.ref().child(`${userObj.uid}/${uuid()}`);
+    const response = await reference.putString(attachment, "data_url");
+    console.log(response);
+    // await dbService.collection("tweet").add({
+    //   text: tweet,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid,
+    // });
+    // setTweet("");
   };
+
+  //file
+
+  const onFileChage = (event) => {
+    const {
+      target: { files },
+    } = event;
+
+    const theFile = files[0]; //파일을 갖는다.
+    const reader = new FileReader(); //reader을 불러온다.
+    reader.onloadend = (event) => {
+      const {
+        currentTarget: { result },
+      } = event;
+      setAttachment(result); //setAttachment 에 저장
+    }; //그림파일을 읽기를 기다린다.
+    reader.readAsDataURL(theFile); //불러온 reader로 파일을 읽는다.
+  };
+
+  //fn
+
+  const onClearPhotoClick = () => setAttachment(null);
 
   return (
     <div>
@@ -48,6 +76,11 @@ const Home = ({ userObj }) => {
           value={tweet}
         />
         <input type="submit" value="트윗보내기" style={{ marginBottom: 20 }} />
+        <input type="file" accept="image/*" onChange={onFileChage} />
+        <div>
+          {attachment && <img src={attachment} width="50px" height="50px" />}
+          <button onClick={onClearPhotoClick}>삭제하기</button>
+        </div>
       </form>
       <div>
         {tweets.map((tweet) => (
