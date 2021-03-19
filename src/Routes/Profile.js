aaa3ee61e95bd7d31f5159237e5c495d0aa5bf06
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-20 04:16:19 +0900

    Create a form so that you can change the user's nickname.

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,12 +1,48 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
 
-const Profile = () => {
-  const onLogoutClick = () => authService.signOut();
+const Profile = ({ userObj, refreshUser }) => {
+  const [newDisplayName, setNewDisplayName] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== "") {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
+
+  const history = useHistory();
+
+  const onHomeClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
   return (
-    <>
-      <button onClick={() => onLogoutClick()}>로그아웃</button>
-    </>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          value={newDisplayName}
+          placeholder="새로운 닉네임을 입력해주세요"
+        />
+        <input type="submit" value="확인" />
+      </form>
+
+      <button onClick={() => onHomeClick()}>로그아웃</button>
+    </div>
   );
 };
 
