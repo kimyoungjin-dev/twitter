002bfe0c9a59a6b2ba516b62098a74e16291e967
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-19 21:56:02 +0900

    Compose the login form screen. By using authService, write the function code by dividing the case of login and the case of membership registration.

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "fbase";
 
 const Login = () => {
-  return <>Login</>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (event) => {
+    const {
+      target: { value, name },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          onChange={onChange}
+          placeholder="Email"
+          required
+          maxLength="20"
+        />
+        <input
+          name="password"
+          type="password"
+          onChange={onChange}
+          placeholder="Password"
+          required
+          maxLength="20"
+        />
+        <input type="submit" value={newAccount ? "계정만들기" : "로그인"} />
+      </form>
+      <div>
+        <button>구글계정 로그인</button>
+        <button>깃허브계정 로그인</button>
+      </div>
+    </>
+  );
 };
 
 export default Login;
