ffaed446160447e56eee6526c10c3987f0f65b70
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-19 22:47:27 +0900

    firebase cannot detect login if login occurs too fast, so use authService.currentUser => authService.onAuthStateChanged.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Router from "./Router";
 import { authService } from "fbase";
+import Loading from "Components/Loading";
 
 const App = () => {
-  const [isLogin, setIsLogin] = useState(authService.currentUser);
-  return <Router isLogin={isLogin} />;
+  const [init, setInit] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLogin(true);
+      } else {
+        setIsLogin(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return <>{init ? <Router isLogin={isLogin} /> : <Loading />}</>;
 };
 
 export default App;
