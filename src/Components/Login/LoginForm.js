6e75f5e4258d134a79abce85450445bda33ad842
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-21 03:45:02 +0900

    LoginForm Design css

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,5 +1,60 @@
 import React, { useState } from "react";
 import { authService } from "fbase";
+import styled from "styled-components";
+import { FaTwitter } from "react-icons/fa";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const LogoTitle = styled.span`
+  font-weight: bold;
+  font-size: 30px;
+  display: block;
+  margin: 20px 0px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  height: 150px;
+  width: 300px;
+  justify-content: space-between;
+`;
+
+const Input = styled.input`
+  padding-left: 20px;
+  background-color: rgb(236, 239, 244);
+  border: none;
+  border-radius: 20px;
+  height: 35px;
+  outline: none;
+
+  ::placeholder {
+    margin-left: 10px;
+    font-size: 16px;
+    color: rgba(0, 0, 0, 0.3);
+  }
+  :last-child {
+    background-color: rgb(109, 195, 239);
+    color: white;
+    font-size: 16px;
+  }
+`;
+
+const Error = styled.span`
+  display: block;
+  margin: 10px 0px;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const ToggleButton = styled.span`
+  display: block;
+  margin-bottom: 10px;
+`;
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -31,9 +86,11 @@ const LoginForm = () => {
     }
   };
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input
+    <Container>
+      <FaTwitter size={50} color={"rgb(109, 195, 239)"} />
+      <LogoTitle>Log in to Twitter</LogoTitle>
+      <Form onSubmit={onSubmit}>
+        <Input
           name="email"
           type="email"
           onChange={onChange}
@@ -41,7 +98,7 @@ const LoginForm = () => {
           required
           maxLength="20"
         />
-        <input
+        <Input
           name="password"
           type="password"
           onChange={onChange}
@@ -49,14 +106,14 @@ const LoginForm = () => {
           required
           maxLength="20"
         />
-        {error}
-        <input type="submit" value={newAccount ? "계정만들기" : "로그인"} />
-      </form>
+        <Input type="submit" value={newAccount ? "Sign up" : "Log in"} />
+      </Form>
 
-      <span onClick={() => setNewAccount((prev) => !prev)}>
-        {newAccount ? "로그인" : "회원가입"}
-      </span>
-    </>
+      <Error>{error}</Error>
+      <ToggleButton onClick={() => setNewAccount((prev) => !prev)}>
+        {newAccount ? "Log in" : "Sign up"}
+      </ToggleButton>
+    </Container>
   );
 };
 
