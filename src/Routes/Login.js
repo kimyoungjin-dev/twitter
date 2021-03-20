c82b6e090ffd866753cbff1c72697e5e6c7f805d
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-21 03:44:51 +0900

    styled css

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -3,7 +3,13 @@ import LoginForm from "Components/Login/LoginForm";
 import Social from "Components/Login/Social";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const Login = () => {
   return (
