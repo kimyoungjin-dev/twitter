ce7f90b8405f808d515ffa97edada4885a7bbbe0
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-21 18:17:35 +0900

    Implement the loading screen.

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,7 +1,21 @@
 import React from "react";
+import LoadingBG from "images/loading.gif";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Loading = () => {
-  return <span>Loading</span>;
+  return (
+    <Container>
+      <img src={LoadingBG} />
+    </Container>
+  );
 };
 
 export default Loading;
